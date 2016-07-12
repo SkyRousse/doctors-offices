78c8d53d1f74a44ff330870c9f5259ea9eefe997
Sebastian Prokuski and Sky Rousse <sky@>
2016-07-12 16:34:06 -0700

    add passing integration spec for adding new patients and folling the route

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -31,23 +31,23 @@ describe('view patient path', {:type => :feature}) do
   it('shows the user the list of patients for a given doctor office in the database') do
     doctors_office = DoctorsOffice.new({:id => nil, :name => 'DR Hollemans Practice', :specialty => 'general care'})
     doctors_office.save()
-    visit('/doctors_office')
+    visit("/doctors_offices/#{doctors_office.id()}")
     expect(page).to have_content('DR Hollemans Practice')
-    patient = Patient.new({:doctors_office_id => 1, :name => 'John', :birthdate => '1990-07-08'})
+    patient = Patient.new({:doctors_office_id => doctors_office.id(), :name => 'John', :birthdate => '1990-07-08'})
     patient.save()
-    visit('/doctors_offices')
+    visit("/patients")
     expect(page).to have_content('John 1990-07-08')
   end
 end
 
-# describe('add office path', {:type => :feature}) do
-#   it('allows user to add a new office') do
-#     visit('/')
-#     click_link('Add New Office')
-#     expect(page).to have_content("Add a Doctor Office")
-#     fill_in('name', :with => 'Kim Practice')
-#     fill_in('specialty', :with => 'Knee Surgery')
-#     click_button('Add Doctor Office')
-#     expect(page).to have_content("Kim Practice Knee Surgery")
-#   end
-# end
+describe('add patient path', {:type => :feature}) do
+  it('allows user to add a new patient') do
+    doctors_office = DoctorsOffice.new({:id => nil, :name => 'DR Hollemans Practice', :specialty => 'general care'})
+    doctors_office.save()
+    visit("/doctors_offices/#{doctors_office.id()}")
+    fill_in('name', :with => 'Kim')
+    fill_in('birthdate', :with => '2001-02-02')
+    click_button('Add Patient')
+    expect(page).to have_content("Kim 2001-02-02")
+  end
+end
